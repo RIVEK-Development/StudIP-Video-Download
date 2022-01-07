66059ab76a15706826af7552619494f211f29dde
RIVEK Development <rivek.development@gmail.com>
2022-01-07 22:30:28 +0100

    not paella player

--- content.js
+++ content.js
@@ -1,13 +1,14 @@
 var vidLink = "https://moritz-riedel.de";
 var url = document.location.href;
 
-if(url.includes("https://opencast-present") && url.includes("paella/ui/") && !url.includes("#iframe")){			//Ist die Seite eine mit Video?
+if(url.includes("https://opencast-present") && (url.includes("paella/ui/") || url.includes("engage/theodul/ui" )) && !url.includes("mode=desktop")){			//Ist die Seite eine mit Video?
 	
 	var ids = url.split("id=");
 	var id = ids[ids.length -1];
 	var VidContainer = "playerContainer_controls_playback_playbackBarPlugins";
+	var VidContainer2 = "vjs-control-bar";
 	
-	createButton(vidLink,"body");
+	createButton(vidLink,document.getElementsByTagName("body")[0],0);
 	
 	var counterOne = setInterval(functionOne, 150);						//Alle 150ms wird geschaut ob Video abgespielt und Player aktiviert wurde
 	function functionOne(){
@@ -16,11 +17,22 @@ if(url.includes("https://opencast-present") && url.includes("paella/ui/") && !ur
 			
 			clearInterval(counterOne);
 			
-			createButton(vidLink, VidContainer);
+			createButton(vidLink, document.getElementById(VidContainer),1);
 		}
+		if (!!document.getElementsByClassName(VidContainer2)[0]){		
+			
+			clearInterval(counterOne);
+			
+			document.getElementsByClassName("videoDisplay")[0].onclick = function(){
+				console.log("clicked")
+				createButton(vidLink, document.getElementsByClassName(VidContainer2)[0],2);
+				document.getElementsByClassName("videoDisplay")[0].onclick = null;
+			}
+		}
+		console.log("still working");
 	}
 	
-	function createButton(Link, div){
+	function createButton(Link, parentObj, appereance){
 	var divID="LinkBtn";
 	if(!!document.getElementById(divID)){
 		document.getElementById(divID).remove();
@@ -28,7 +40,7 @@ if(url.includes("https://opencast-present") && url.includes("paella/ui/") && !ur
 	var button = document.createElement("div");				//erstellt Container
 	var imgH = " 22px;";
 	var LinkImgSrc = "download.svg";
-	if(div.match("body")){
+	if(appereance == 0){
 		button.style="position: absolute;"+
 			"left: 0.7%;"+
 			"bottom: 1%;"+
@@ -37,12 +49,22 @@ if(url.includes("https://opencast-present") && url.includes("paella/ui/") && !ur
 		imgH = " 100%; cursor: pointer;";
 		LinkImgSrc = "download_round.svg";
 	}
-	else{
+	else if(appereance == 1){
 		button.className="buttonPlugin left showPlaybackRateButton";
+	}
+	else{
+		button.className="vjs-play-control vjs-control vjs-button vjs-paused";
+		button.style="padding-top: 5px;";
+		imgH = " 65%;";
 	}
 	button.title="Download";
 	button.id =divID;
-	document.getElementById(div).appendChild(button);		//Fügt den Container den vorhandenen Elementen hinzu
+	if(appereance == 2){
+		parentObj.insertBefore(button, parentObj.children[9]);
+	}
+	else{
+		parentObj.appendChild(button);		//Fügt den Container den vorhandenen Elementen hinzu
+	}
 	var link = document.createElement("img");																	//erstellt Downlaod-Icon
 	link.src =chrome.runtime.getURL(LinkImgSrc);
 	link.id= "VideoLink"; 
--- content2.js
+++ content2.js
@@ -10,9 +10,9 @@ function createBtn(){
 		button.title="Download";
 		button.id ="LinkBtn_"+i;
 		button.setAttribute('type', 'button');
-		button.style ='font-size: 20px; cursor: pointer;';
+		button.style ='font-size: 20px; cursor: pointer; display: block;';
 		button.innerText = ("Download");
-		episodes[i].appendChild(button);		
+		episodes[i].children[1].appendChild(button);		
 							
 		button.onclick = function click(clicked){
 			var id = clicked.target.id.split("_")[1];
@@ -26,4 +26,20 @@ function download(id){
 	chrome.runtime.sendMessage({json: id, download: "true"}, function(response) {
 		console.log(response.videoName);
 	});
+}
+
+
+var buttonA = document.createElement("button");				
+buttonA.title="Download all";
+buttonA.id ="LinkBtn_All";
+buttonA.setAttribute('type', 'button');
+buttonA.style ='font-size: 20px; cursor: pointer; display: block;';
+buttonA.innerText = ("Download all videos");
+document.getElementsByTagName("form")[0].appendChild(buttonA);		
+
+buttonA.onclick = function clickAll(clicked){
+	for (i=0; i<episodes.length; i++){
+		var videoID = episodes[i].id;
+		download(videoID);
+	}
 }
